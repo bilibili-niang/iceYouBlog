e5248bdbdde923fe529085555cd96cf756d66e98
张嘉凯 <killicestone@126.com>
2023-08-01 09:32:06 +0800

    feat,master删除无用资源

--- behind/src/test/writeMarkdownByUrl.js
+++
@@ -1,6 +0,0 @@
-const {writeToDatabaseByUrl} = require('../services/tool.service');
-
-const url = 'https://blog.csdn.net/ice_stone_kai/article/details/110456464'
-
-writeToDatabaseByUrl(url)
-
--- behind/src/tools/formateMyTime.js
+++
@@ -1,4 +0,0 @@
-/* @author icestone , 8:27
- * @date 2023/5/12
- * TODO 格式化我的文章时间
-*/
