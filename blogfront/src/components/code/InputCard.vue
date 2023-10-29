7a4619dce41ae35772e5b6163924d8a51b671482
张嘉凯 <killicestone@126.com>
2023-10-29 23:48:37 +0800

    feat
    修改忽略文件

--- blogfront/src/components/code/InputCard.vue
+++ blogfront/src/components/code/InputCard.vue
@@ -1,130 +1,99 @@
 <template>
   <div class="inputCard">
-    <el-button @click="drawer = true" class="m-normal">add code</el-button>
-    <el-drawer v-model="drawer" title="新增code" size="70%" direction="ttb" :with-header="false">
-      <div class="form">
-        <div class="des">
-          <div class="desLim m-b">
-            <el-text class=m-r>title</el-text>
-            <el-input v-model="form.title" autocomplete="off"/>
-            <el-text class=m-r>tag1</el-text>
-            <el-input v-model="form.tag1" autocomplete="off"/>
-          </div>
-          <div class="desLim m-b">
-            <el-text class=m-r>tag2</el-text>
-            <el-input v-model="form.tag2" autocomplete="off"/>
-            <el-text class=m-r>tag3</el-text>
-            <el-input v-model="form.tag3" autocomplete="off"/>
-          </div>
+    <ice-button @click="drawer = true" class="m-normal">add code</ice-button>
+    <ice-column v-show="drawer">
+      <ice-column>
+        <ice-column>
+          <ice-row>
+            <ice-input v-model="form.title" placeholder="title" class="m-r"/>
+            <ice-input v-model="form.tag1" placeholder="tag1" class="m-r"/>
+            <ice-input v-model="form.tag2" placeholder="tag2" class="m-r"/>
+            <ice-input v-model="form.tag3" placeholder="tag3" class="m-r"/>
+          </ice-row>
           <el-text>description</el-text>
-          <el-input v-model="form.description" autocomplete="off"/>
-        </div>
+          <el-input v-model="form.description"/>
+        </ice-column>
         <v-md-editor
             :include-level="[3,4,5]"
             @save="drawer = true"
             v-model="form.content"
             :disabled-menus="[]"
         ></v-md-editor>
-      </div>
+      </ice-column>
 
       <div class="btns">
-        <el-button @click="submitCode()">submit</el-button>
-        <el-button @click="showInput()">close</el-button>
+        <ice-button @click="submitCode()">submit</ice-button>
+        <ice-button @click="showInput()">close</ice-button>
       </div>
-    </el-drawer>
+    </ice-column>
   </div>
 </template>
 
 <script>
-import http from '@/common/api/request'
-import { ElMessage } from 'element-plus'
-import { h } from 'vue'
+import http from "@/common/api/request";
+import {ElMessage} from "element-plus";
+import {h} from "vue";
 
 export default {
   name: "InputCard",
-  data () {
+  data() {
     return {
       showFlag: false,
       form: {
-        title: 'my title',
-        tag1: 'css',
-        tag2: 'html',
-        tag3: 'code',
+        title: "my title",
+        tag1: "css",
+        tag2: "html",
+        tag3: "code",
         content: `> type your first code`,
-        description: 'description about this code'
+        description: "description about this code"
       },
       drawer: false
-    }
+    };
   },
   methods: {
-    alertMessage (title, sub, color) {
-      const useColor = color || 'red'
+    alertMessage(title, sub, color) {
+      const useColor = color || "red";
       ElMessage({
-        message: h('p', null, [
-          h('span', null, title),
-          h('i', { style: `color: ${ useColor }` }, sub),
+        message: h("p", null, [
+          h("span", null, title),
+          h("i", {style: `color: ${useColor}`}, sub),
         ]),
-      })
+      });
     },
     /* @author icestone , 17:27
      * @date 2023/5/16
      * 提交
     */
-    submitCode () {
-      console.log("this.form:")
-      console.log(this.form)
+    submitCode() {
       http.$axios({
-        url: '/code/codeClips',
-        method: 'POST',
+        url: "/code/codeClips",
+        method: "POST",
         headers: {
           token: true
         },
         data: this.form
       })
           .then(res => {
-            this.alertMessage(res.message)
+            this.alertMessage(res.message);
             if (res.success) {
               // 成功
-              console.log(res)
-              this.showFlag = false
-              this.form = null
+              this.showFlag = false;
             } else {
               // 失败
-              console.log(res)
+              console.log(res);
             }
           })
           .catch(e => {
-            console.log(e)
-          })
+            console.log(e);
+          });
     },
-    showInput () {
-      console.log('showInput')
-      this.showFlag = !this.showFlag
+    showInput() {
+      console.log("showInput");
+      this.showFlag = !this.showFlag;
     }
   }
-}
+};
 </script>
 
 <style scoped lang="less">
-.inputCard {
-  :deep .el-input {
-    width: auto;
-    margin-right: .3rem;
-  }
-
-  .des {
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: row;
-
-    .desLim {
-      display: flex;
-      flex-direction: row;
-    }
-  }
-
-  .v-md-editor {
-    min-height: 40vh;
-  }
-}
 </style>
