1108f83bca78d063d4a49b4953c8233e6e1b506c
张嘉凯 <killicestone@126.com>
2024-08-23 16:37:00 +0800

    feat: :sparkles: 添加Prettier的支持

--- blogfront/src/views/Login/login.vue
+++ blogfront/src/views/Login/login.vue
@@ -18,57 +18,57 @@
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
-import { useRouter } from "vue-router";
-import http from "@/api/request";
-import { fun } from "@/hook/function";
-import User from "@/api/user";
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import http from '@/api/request'
+import { fun } from '@/hook/function'
+import User from '@/api/user'
 
 let login = reactive({
-  username: "",
-  password: "",
-});
+  username: '',
+  password: ''
+})
 
-const router = useRouter();
+const router = useRouter()
 const submit = async () => {
   if (login.username.length < 1 || login.password.length < 1) {
-    fun.alert("请输入正确账号/密码");
+    fun.alert('请输入正确账号/密码')
   } else {
     await User.login(login)
-      .then((res) => {
+      .then(res => {
         if (res.success) {
-          fun.alert(res.message);
+          fun.alert(res.message)
           // 登陆成功
-          const userInfo = res.result || "";
+          const userInfo = res.result || ''
           // 写入token
-          localStorage.setItem("userInfo", JSON.stringify(userInfo));
+          localStorage.setItem('userInfo', JSON.stringify(userInfo))
           // 跳转user页面
-          router.push({ path: "/user" });
+          router.push({ path: '/user' })
         } else {
-          fun.alert(res.message);
+          fun.alert(res.message)
         }
       })
-      .catch((e) => {
-        console.log("e:");
-        console.log(e);
-      });
+      .catch(e => {
+        console.log('e:')
+        console.log(e)
+      })
   }
-};
+}
 
 // 验证用户是否登录,如果登录,跳转个人页面
 const verifyLogin = () => {
-  const user = localStorage.getItem("userInfo");
+  const user = localStorage.getItem('userInfo')
   // 没有用户登录
   if (JSON.stringify(user).length < 10) {
-    localStorage.removeItem("userInfo");
-    return;
+    localStorage.removeItem('userInfo')
+    return
   } else {
     // 有用户登录,跳转user页面
-    router.push({ path: "/user" });
+    router.push({ path: '/user' })
   }
-};
+}
 
-verifyLogin();
+verifyLogin()
 </script>
 
 <style scoped lang="less">
